58500acaf3ca3b7e0f5f55c69054436fb22c29c9
tknawara <tarek@knawara.dev>
2026-08-23 21:02:56 +0100

    rootle 0.3.2

--- Formula/rootle.rb
+++ Formula/rootle.rb
@@ -1,8 +1,8 @@
 class Rootle < Formula
   desc "Modal TUI for browsing remote source-control systems from the terminal"
   homepage "https://rootle.dev/"
-  url "https://static.crates.io/crates/rootle/rootle-0.3.1.crate"
-  sha256 "978691cd60857bd53c65ab4a8e3c5683e9e77ca4d09b052a2c9b84eb438699d9"
+  url "https://static.crates.io/crates/rootle/rootle-0.3.2.crate"
+  sha256 "38cb276dea1dd3f9654602d88059df5616e139e2e37b7d9b2e6a09f656545928"
   license "MIT"
 
   depends_on "cmake" => :build # aws-lc-sys
